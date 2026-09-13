4e9867d2905c8cfe45c8c045dd7192f6ed3d6a45
Kukemanov Daniil <danilbelov900@gmail.com>
2026-09-13 16:35:14 +0800

    Update clouds.gd

--- assets/common/environment/clouds/clouds.gd
+++ assets/common/environment/clouds/clouds.gd
@@ -3,7 +3,6 @@ extends Node3D
 func _process(_delta: float) -> void:
 	var camera := get_viewport().get_camera_3d()
 	if camera:
-		# Плавное следование, чтобы не было рывков шума
 		var target_pos = camera.global_position
-		target_pos.y = 0.0 # Высота облаков
+		target_pos.y = 0.0
 		global_position = target_pos
