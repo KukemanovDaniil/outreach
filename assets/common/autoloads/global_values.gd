b97bb5c27bf7bd433cb62db942d13283e3142b23
Kukemanov Daniil <danilbelov900@gmail.com>
2026-09-13 16:30:20 +0800

    Update global_values.gd

--- assets/common/autoloads/global_values.gd
+++ assets/common/autoloads/global_values.gd
@@ -6,7 +6,6 @@ var version : Variant = "v" + str(ProjectSettings.get_setting("application/confi
 
 var time : float = 12
 
-# Сиды для генерации
 var world_seed : int = 0
 var cave_seed : int = 0
 var tree_seed : int = 0
@@ -29,21 +28,19 @@ var fog : bool = true
 var post_processing : bool = SaveSystem.get_val("options", "post_processing", true)
 var environment : bool = true
 
-var current_block : int = 1 # Лучше поставить 1 (камень/земля), чтобы не ставить "воздух" (0)
+var current_block : int = 1
 
 var fov : int = SaveSystem.get_val("options", "fov", 75)
 
 var max_fov : int = 145
 var min_fov : int = 1
 
-# V-Sync
 var v_sync : bool = SaveSystem.get_val("options", "v_sync", true)
 
 var music_volume : float = SaveSystem.get_val("audio", "music_volume", 1.0)
 var sfx_volume : float = SaveSystem.get_val("audio", "sfx_volume", 1.0)
 
 func _ready() -> void:
-	# Применяем V-Sync сразу при запуске игры
 	var mode = DisplayServer.VSYNC_ENABLED if v_sync else DisplayServer.VSYNC_DISABLED
 	DisplayServer.window_set_vsync_mode(mode)
 	
