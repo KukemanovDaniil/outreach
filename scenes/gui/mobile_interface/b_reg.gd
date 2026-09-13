cbbc4fae735c0fd1eb142ee00833807d5106900a
Kukemanov Daniil <danilbelov900@gmail.com>
2026-09-13 17:14:00 +0800

    Update b_reg.gd

--- scenes/gui/mobile_interface/b_reg.gd
+++ scenes/gui/mobile_interface/b_reg.gd
@@ -1,9 +1,7 @@
 extends Button
 
-# Включает режим "проскальзывания" (как в TouchScreenButton)
 @export var passby: bool = false
 
 func _ready() -> void:
-	# Регистрируем кнопку в глобальной системе
 	ButtonManager.register_button(self)
 	
