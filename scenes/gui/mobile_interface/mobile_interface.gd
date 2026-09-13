12d2570b76bfd5cba135cec5594f3609ef0ed5cf
Kukemanov Daniil <danilbelov900@gmail.com>
2026-09-13 17:14:56 +0800

    Update mobile_interface.gd

--- scenes/gui/mobile_interface/mobile_interface.gd
+++ scenes/gui/mobile_interface/mobile_interface.gd
@@ -1,7 +1,6 @@
-extends CanvasLayer # Скрипт на родителе всех кнопок
+extends CanvasLayer
 
 func _ready() -> void:
-	# Если это ПК — сносим к чертям весь этот узел со всеми потрохами
-	if OS.get_name() in ["Wndows", "Linux"]:
+	if OS.get_name() in ["Windows", "Linux"]:
 		queue_free()
-		return # Дальше код не пойдет
+		return
