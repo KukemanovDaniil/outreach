b74f1bef1567a28f99f3078c69b4ad46e0516585
Kukemanov Daniil <danilbelov900@gmail.com>
2026-09-13 16:31:08 +0800

    Update save_system.gd

--- assets/common/autoloads/save_system.gd
+++ assets/common/autoloads/save_system.gd
@@ -6,13 +6,11 @@ var config = ConfigFile.new()
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
 
-# Сохранить любое значение (bool, int, float, String)
 func set_val(section: String, key: String, value):
-	config.load(PATH) # Загружаем текущий файл, чтобы не затереть другие настройки
+	config.load(PATH)
 	config.set_value(section, key, value)
 	config.save(PATH)
 
-# Загрузить значение (с дефолтным ответом, если файла еще нет)
 func get_val(section: String, key: String, default):
 	config.load(PATH)
 	return config.get_value(section, key, default)
