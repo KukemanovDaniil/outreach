1ec169d3427ddc9159150ad6b5910f9a357baf43
Kukemanov Daniil <danilbelov900@gmail.com>
2026-09-13 16:34:49 +0800

    Update window_manager.gd

--- assets/common/autoloads/window_manager.gd
+++ assets/common/autoloads/window_manager.gd
@@ -10,11 +10,7 @@ func _ready():
 func open_window(scene: PackedScene):
 	if not scene: return
 	
-	# ПРОВЕРКА: Не открываем ли мы то же самое окно еще раз?
-	# Если в стеке уже есть окна, лучше сначала закрыть старое или просто выйти
 	if not window_stack.is_empty():
-		# Опционально: если хочешь только ОДНО окно за раз
-		# close_last_window() 
 		return 
 
 	var win = scene.instantiate()
@@ -30,17 +26,13 @@ func close_last_window():
 		if is_instance_valid(win):
 			win.queue_free()
 	
-	# Проверяем стек еще раз, чтобы убедиться, что всё чисто
 	if window_stack.is_empty():
 		get_tree().paused = false
-		# В МЕНЮ мы не должны прятать мышь! 
-		# Проверь, находимся ли мы в игре, прежде чем делать CAPTURED
 		if get_tree().current_scene.name != "menu":
 			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		else:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
-# Добавим функцию полной очистки для безопасности при смене сцен
 func close_all_windows():
 	for win in window_stack:
 		if is_instance_valid(win):
