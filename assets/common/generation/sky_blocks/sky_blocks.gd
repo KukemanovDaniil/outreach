cc58e8dc185caa5b137b9b6850e08a0a8588cb11
Kukemanov Daniil <danilbelov900@gmail.com>
2026-09-13 16:40:09 +0800

    Update sky_blocks.gd

--- assets/common/generation/sky_blocks/sky_blocks.gd
+++ assets/common/generation/sky_blocks/sky_blocks.gd
@@ -10,18 +10,16 @@ func _get_used_channels_mask() -> int:
 
 func _generate_block(buffer: VoxelBuffer, origin: Vector3i, _lod: int) -> void:
 	if block_lib == null: return
-	var models_count = block_lib.models.size() # Быстрее, чем .size() массива
+	var models_count = block_lib.models.size()
 	if models_count == 0: return
 
 	var size = buffer.get_size()
 	var seed_val = GlobalValues.world_seed
 
-	# Находим стартовые смещения внутри чанка, чтобы попасть в глобальную сетку grid_step
 	var start_x = (grid_step - (origin.x % grid_step)) % grid_step
 	var start_y = (grid_step - (origin.y % grid_step)) % grid_step
 	var start_z = (grid_step - (origin.z % grid_step)) % grid_step
 
-	# Прямые прыжки по сетке без проверки if % grid_step
 	var z = start_z
 	while z < size.z:
 		var gz = origin.z + z
@@ -32,9 +30,7 @@ func _generate_block(buffer: VoxelBuffer, origin: Vector3i, _lod: int) -> void:
 			while x < size.x:
 				var gx = origin.x + x
 				
-				# Псевдорандом через битовые сдвиги (быстрее hash(Vector3i))
 				var h = (gx * 73856093) ^ (gy * 19349663) ^ (gz * 83492791) ^ seed_val
-				# Избегаем отрицательных чисел и берем ID
 				var random_block_id = (h & 0x7FFFFFFF) % models_count + 1
 				
 				buffer.set_voxel(random_block_id, x, y, z, _CHANNEL)
