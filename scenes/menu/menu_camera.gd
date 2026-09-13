19aa3f55f73e7471af5857ecc83b775db435a6cb
Kukemanov Daniil <danilbelov900@gmail.com>
2026-09-13 17:17:45 +0800

    Update menu_camera.gd

--- scenes/menu/menu_camera.gd
+++ scenes/menu/menu_camera.gd
@@ -6,10 +6,8 @@ func _ready() -> void:
 func start_rotation_tween() -> void:
 	var tween = create_tween()
 	
-	# Настраиваем бесконечное вращение на 360 градусов (TAU)
 	tween.tween_property(self, "rotation:y", rotation.y + TAU, 4.0)\
 		.set_trans(Tween.TRANS_LINEAR)\
 		.set_ease(Tween.EASE_IN_OUT)
 		
-	# Зацикливаем
 	tween.set_loops()
